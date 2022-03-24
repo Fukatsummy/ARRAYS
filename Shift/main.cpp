7a8bd97086fcba9da563988fbab57729e6ffd2d1
Влад Безедика <sttasea@mail.ru>
2022-03-24 21:26:29 +0300

    shift encoding

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+п»ї#include<iostream>
 using namespace std;
 
 void main()
@@ -6,16 +6,16 @@ void main()
 	setlocale(LC_ALL, "");
 	const int n = 10;
 	int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
-	//Вывод исходного массива на экран:
+	//Р’С‹РІРѕРґ РёСЃС…РѕРґРЅРѕРіРѕ РјР°СЃСЃРёРІР° РЅР° СЌРєСЂР°РЅ:
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
 
-	//Циклический сдвиг массива:
+	//Р¦РёРєР»РёС‡РµСЃРєРёР№ СЃРґРІРёРі РјР°СЃСЃРёРІР°:
 	int number_of_shifts;
-	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЃРґРІРёРіРѕРІ: "; cin >> number_of_shifts;
 	for (int i = 0; i < number_of_shifts; i++)
 	{
 		int buffer = arr[0];
@@ -25,7 +25,7 @@ void main()
 		}
 		arr[n - 1] = buffer;
 	}
-	//Вывод сдвинутого массива на экран:
+	//Р’С‹РІРѕРґ СЃРґРІРёРЅСѓС‚РѕРіРѕ РјР°СЃСЃРёРІР° РЅР° СЌРєСЂР°РЅ:
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
